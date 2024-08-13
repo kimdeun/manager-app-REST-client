0244b3875a10dda7a1050fd0c75ec56dbb308d7c
De Un Kim <deunkim@MacbookM1.local>
2024-08-13 17:17:15 +0300

    added oauth2 client

--- src/main/java/com/example/manger_app/config/ClientBeans.java
+++ src/main/java/com/example/manger_app/config/ClientBeans.java
@@ -1,22 +1,29 @@
 package com.example.manger_app.config;
 
 import com.example.manger_app.client.RestClientProductsRestClient;
+import com.example.manger_app.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientBeans  {
     @Bean
-    public RestClientProductsRestClient restClientProductsRestClient(
+    public RestClientProductsRestClient productsRestClient(
             @Value("${services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
-            @Value("${services.catalogue.username:}") String catalogueUserName,
-            @Value("${services.catalogue.password:}") String cataloguePassword) {
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository,
+            @Value("${services.catalogue.registration-id:keycloak}") String registrationId) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
-                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUserName, cataloguePassword) )
+                .requestInterceptor(new OAuthClientHttpRequestInterceptor(
+                        new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                oAuth2AuthorizedClientRepository), registrationId))
                 .build());
     }
 }
